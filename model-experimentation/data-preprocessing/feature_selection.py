632fb0dc43eb6ac817c52182c99241da02be4832
Sergio Gonzalez Barraza <114702763+SergioGzzBrz@users.noreply.github.com>
2026-05-24 12:28:18 -0600

    Remove feature selection from correlation due to features not being deleted by the process. Moved to justification notebook

--- model-experimentation/data-preprocessing/feature_selection.py
+++ model-experimentation/data-preprocessing/feature_selection.py
@@ -12,24 +12,3 @@ def variance_treshold_selection(df: pd.DataFrame) -> pd.DataFrame:
     # TODO, USE: from sklearn.feature_selection import VarianceThreshold
 
     return df
-
-
-def correlation_based_selection(
-    df: pd.DataFrame, threshold: float = 0.90
-) -> pd.DataFrame:
-    """
-    Correlation-based feature selection identifies and removes features that are highly correlated with each other.
-    The idea is that if two features are highly correlated, they may provide redundant information to the model.
-    This method helps to reduce multicollinearity and can improve model performance.
-
-    Parameters:
-    - df: Input DataFrame containing the features.
-    - threshold: Correlation threshold above which features will be considered redundant and removed.
-
-    Returns:
-    - A DataFrame with redundant features removed based on the specified correlation threshold.
-    """
-    # TODO, seems like there is not very high correlation between features, so we will not remove anything here
-    # I will change this into a different notebook so that we can add eveidence of why we did not do this
-    # @SergioGzzBrz
-    return df
