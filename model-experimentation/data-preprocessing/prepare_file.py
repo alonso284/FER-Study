38d76e193b54a3c56b63e7c686fb1b9e54c9a621
Luis <lamadoalvarez@gmail.com>
2026-05-25 22:32:14 -0600

    fix clean file preparation

--- model-experimentation/data-preprocessing/prepare_file.py
+++ model-experimentation/data-preprocessing/prepare_file.py
@@ -10,10 +10,13 @@ def generate_prepared_file(file_name: str = "../../Clean_NPFC-TEST_Database_V2.c
   start_time = time.perf_counter()
 
   df = data_preparation.align_lag_signals(df)
-  df = data_preparation.prepare_data(df)
   df = data_preparation.normalize_signals_with_mediation_baseline(df)
+  df = data_preparation.prepare_data(df)
 
   end_time = time.perf_counter()  
 
   df.to_csv(file_name)
   print(f"File created: {file_name} ({end_time - start_time:0.4f}s)")
+
+if __name__ == "__main__":
+  generate_prepared_file()
